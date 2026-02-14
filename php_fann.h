2b0ce9a2322964d968718b7610fd5fb175f88ea4
rernesto <rernesto.espinosa@gmail.com>
2026-02-14 21:32:57 +0000

    chore: extension version updated

--- php_fann.h
+++ php_fann.h
@@ -6,7 +6,7 @@
 extern zend_module_entry fann_module_entry;
 # define phpext_fann_ptr &fann_module_entry
 
-# define PHP_FANN_VERSION "0.1.0"
+# define PHP_FANN_VERSION "1.0.0"
 
 # if defined(ZTS) && defined(COMPILE_DL_FANN)
 ZEND_TSRMLS_CACHE_EXTERN()
